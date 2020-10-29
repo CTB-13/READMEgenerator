013fba5ca1b4d5d2d404bef18b7b4cd38f959271
CTB-13 <68928283+CTB-13@users.noreply.github.com>
2020-10-29 11:44:38 -0400

    Add files via upload

---
+++ README generator/index.js
@@ -0,0 +1,92 @@
+import { writeFile } from "fs";
+import pkg from 'inquirer';
+import generateMarkdown from "./utilities/generateMarkdown.js";
+
+const { prompt } = pkg;
+
+const questions = [
+    {
+        type: "input",
+        message: "What is the project title?",
+        name: "Title"
+    },
+    {
+        type: "input",
+        message: "Describe the project",
+        name: "Description"
+    },
+    {
+        type: "input",
+        message: "How do you go about installing the  project?",
+        name: "Installation"
+    },
+    {
+        type: "input",
+        message: "Provide instructions for use.",
+        name: "Usage"
+    },
+    {
+        type: "list",
+        message: "Select license",
+        name: "License",
+        choices: [
+            "MIT",
+            "GVL-GPL 3.0",
+            "APACHE 2.0",
+            "BSD 3",
+            "None"
+        ]
+    },
+    {
+        type: "input",
+        message: "Contributors?",
+        name: "Contributors"
+    },
+    {
+        type: "input",
+        message: "How do you test your project?",
+        name: "Test"
+    },
+    
+    {
+        type: "input",
+        message: "Contact info for further questions",
+        name: "Questions"
+    },
+    {
+        type: "input",
+        message: "Your Github username",
+        name: "Username"
+    },
+    {
+        type: "input",
+        message: "What is your email?",
+        name: "Email"
+    }
+    
+];
+
+function writeToFile(fileName, data) {
+    writeFile(fileName, data, err => {
+        if (err) {
+          throw err;
+        }
+      });
+    }
+
+
+function init() {
+    prompt(questions).then(answers => {
+        
+        const response = generateMarkdown(answers);
+        console.log(answers);
+       
+        writeToFile("README.md", response);
+      
+    })
+    
+
+}
+
+
+init();
